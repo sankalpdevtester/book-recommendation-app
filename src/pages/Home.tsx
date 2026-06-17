f262e1bd6d26b64dfa8f9bd4660aa4b469606ebc
sankalpdevtester <ohofufic0@gmail.com>
2026-06-17 19:35:18 +0530

    feat: home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Book Recommendation Web App</h1>
+      <p>Search for books, get personalized recommendations, and track your reading history.</p>
+    </div>
+  );
+}
+
+export default Home;
