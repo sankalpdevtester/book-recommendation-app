5c620eed77a251b7b82636c44240e7d3be72ab48
sankalpdevtester <ohofufic0@gmail.com>
2026-06-18 17:56:30 +0530

    feat: add user model

---
+++ src/server/models/User.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ nullable: true })
+  name?: string;
+
+  @Column({ nullable: true })
+  bio?: string;
+}
